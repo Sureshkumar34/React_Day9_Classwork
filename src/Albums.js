b06ea79179037763e0dc23f2472ce2ca25b61652
Sureshkumar34 <128037589+Sureshkumar34@users.noreply.github.com>
2023-04-05 08:22:21 +0530

    Add files via upload

---
+++ src/Albums.js
@@ -0,0 +1,16 @@
+import React from 'react'
+
+function Albums() {
+  return (
+    <div>
+        <h4>Albums1</h4>
+        <h4>Singer1</h4>
+        <h4>Albums2</h4>
+        <h4>Singer2</h4>
+        <h4>Albums3</h4>
+        <h4>Singer3</h4>
+        </div>
+  )
+}
+
+export default Albums
